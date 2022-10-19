215b67c1b1f2ba1cdb2acf3ba2f45454f5b5dde6
J-Cundiff <jd.cundiff@outlook.com>
2022-10-19 15:47:56 -0500

    Added Race and weapon type

--- DungeonLibrary/Weapon.cs
+++ DungeonLibrary/Weapon.cs
@@ -7,13 +7,41 @@ using System.Threading.Tasks;
 namespace DungeonLibrary
 {
     public class Weapon
+        //type - WeaponType
     {
+        //minDamage – int
+
+        //maxDamage – int
+
+        //name – string
+
+        //bonusHitChance – int
+
+        //isTwoHanded - bool
+
+
+
+        //Full Qualified CTOR
+
+        //Nicely Formatted ToString()
+
+
+
+        
+
+       
+        //FIELDS --- funny
+
+        
+        private WeaponType _type;
         private int _minDamage;
         private int _maxDamage;
-        private string _name;
+        private string? _name;
         private int _bonusHitChance;
         private bool _isTwoHanded;
 
+        //PROPERTIES --- people
+        
         public string Name
         {
             get { return _name; }
@@ -29,44 +57,66 @@ namespace DungeonLibrary
             get { return _isTwoHanded; }
             set { _isTwoHanded = value; }
         }
-        public int MaxDamage
+        public WeaponType Type
         {
-            get { return _maxDamage; }
-            set { _maxDamage = value; }
+            get { return _type; }
+            set { _type = value; }
         }
+        
         public int MinDamage
         {
             get { return _minDamage; }
             set
             {
-                if (value < 0 && value <= MaxDamage)
+               _minDamage = (value > 0 && value <= MaxDamage ? value : 1);
+                //If the value passed is greater than 0 && less than less than ot equal to max damage,
+                //assisgn that value to _minDamage.Otherwise, assign 1 to _minDamage
+                //this.MinDamage++, += 5, could send it over MaxDamage, we don't want that
+                /*
+                if (value > 0 && value <= MaxDamage)
                 {
-                    _minDamage = value;
+                   _minDamage = value;
                 }
                 else
                 {
                     _minDamage = 1;
-                }
+                }*/
+
             }
         }
+        public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set { _maxDamage = value; }
+        }
 
+        //CONSTRUCTORS --- collect
         public Weapon() { }
 
-        public Weapon(int minDamage, int maxDamage, string name, int bonusHitChance, bool isTwoHanded)
+        public Weapon(int maxDamage, string name, int bonusHitChance, bool isTwoHanded, WeaponType type, int minDamage)
         {
+            //Property = parameter
+            //PascalCase = camelCase
+            //Any other properties with business rules that rely on other properties MUST come AFTER
+            //those properties are set.
             MaxDamage = maxDamage;
-            MinDamage = minDamage;
             Name = name;
             BonusHitChance = bonusHitChance;
             IsTwoHanded = isTwoHanded;
+            Type = type;
+            MinDamage = minDamage;
+           
         }
 
+        //METHODS --- monkeys
         public override string ToString()
         {
-            return string.Format("{0}\t{1} to {2} Damage\n" +
-                "Bonus Hit: {3}%\t{4}",
-                Name, MinDamage, MaxDamage, BonusHitChance, IsTwoHanded ? "Two-Handed" : "One-Handed");
-        }//end ToString
+            return $"{Name}\t{MinDamage} to {MaxDamage} Damage\n" +
+                $"Bonus Hit: {BonusHitChance}%\n" + 
+                $"Type: {Type}\t\t{(IsTwoHanded ? "Two-Handed" : "One-Handed")}";
+        }//end ToString 
+        
+        
 
     }
 }
