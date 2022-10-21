ffe34274703c1b420e0dfaabb4af11a1bc012a83
J-Cundiff <jd.cundiff@outlook.com>
2022-10-21 12:47:47 -0500

    build 4 different implementations of monster

---
+++ DungeonLibrary/Rabbit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Rabbit : Monster
+    {
+        //FIELDS --- funny
+        // no fields
+
+        //PROPERTIES --- people
+        public bool IsFluffy { get; set; }
+
+        //CONSTRUCTORS --- collect
+        //Default ctor to set some basic values for a generic monster of this type
+        public Rabbit()
+        {
+            MaxLife = 6;
+            MaxDamage = 3;
+            Name = "Baby Rabbit";
+            Life = MaxLife;
+            HitChance = 20;
+            Block = 20;
+            MinDamage = 1;
+            Description = "It's just a cte little bunny.... Why would you hurt it?";
+            IsFluffy = false;
+        }
+        //Parent compliment (Monster) ctor
+        //Intellisense quick action on the Parent name in the class declaration.
+        public Rabbit(string name, int hitChance, int block, //Character
+            int maxLife, int maxDamage, int minDamage, string description,//Monster
+            bool isFluffy) //Rabbit
+            : base(name, hitChance, block, maxLife, maxDamage, minDamage, description)
+        {
+            IsFluffy = isFluffy;
+        }
+
+
+        //METHODS --- monkeys
+
+        public override string ToString()
+        {
+            return base.ToString() + (IsFluffy ? "It's Fluffy!!" : "It's not fluffy");
+        }
+        //Character CalcBlock = Block
+        //Monster CalcBlock = Block
+        //Rabbit CalcBlock = not Block
+        public override int CalcBlock()
+        {
+            int calculatedBlock = Block;
+            //Apply a 50% increase to the rabbit's if it's fluffy
+            if (IsFluffy)
+            {
+                calculatedBlock += calculatedBlock / 2;
+            }
+            return calculatedBlock;
+        }
+    }
+}
---
+++ DungeonLibrary/Turtle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Turtle : Monster
+    {
+
+        //FIELDS --- funny
+
+
+
+        //PROPERTIES --- people
+        public int BonusBlock { get; set; }
+        public int HidePercent { get; set; }
+
+
+        //CONSTRUCTORS --- collect
+        public Turtle(string name, int hitChance, int block, int maxLife, int maxDamage, int minDamage, string description, int bonusBlock, int hidePercent) 
+            : base(name, hitChance, block, maxLife, maxDamage, minDamage, description)
+        {
+            //Pass everthing from Monster back to Monster : base.
+            //Handle unique turtlie things here
+            Block = bonusBlock;
+            HidePercent = hidePercent;
+        }
+        public Turtle()
+        {
+            //SET MAX VALUES FIRST!
+            MaxLife = 6;
+            MaxDamage = 3;
+            Life = 6;
+            MinDamage = 0;
+            HitChance = 5;
+            Block = 10;
+            Name = "Baby Turtle";
+            Description = "It's a cute baby turtle... keep it " +
+                "away from the ooze.";
+            BonusBlock = 5;
+            HidePercent = 10;
+
+        }//end default
+
+        //METHODS --- monkeys
+        public override string ToString()
+        {
+            return base.ToString() + $"\n{HidePercent}% chance it will hide, granting {BonusBlock} bonus block.";
+        }
+        public override int CalcBlock()
+        {
+            int calculatedBlock = Block;
+            //Generate a random percent from 0 - 100
+            int percent = new Random().Next(101);
+            if (HidePercent >= percent)
+            {
+                //it's a a success let the turtle hide
+                calculatedBlock += BonusBlock;
+            }
+            return calculatedBlock;
+        }
+
+
+
+    }
+}
